7aa0996fae25432074094bd60dd2b3a15c47e82b
Kang Wang <kang.wang@hakai.org>
2018-03-27 16:50:23 -0700

    Moved request function into geocode.js & refactored app.js by removing code to make request (imported geocode function, instead).

--- app.js
+++ app.js
@@ -15,21 +15,6 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-geocode.geocodeAddress(argv.a);
+const inputAddress = argv.a;
 
-const encodedUserAddress = encodeURIComponent(argv.a);
-
-request({
-  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedUserAddress}`,
-  json: true
-}, (err, res, body) => {
-  if (err) {
-    console.log('\nUnable to connect to Google servers.\n');
-  } else if (body.status === 'ZERO_RESULTS') {
-    console.log('\nUnable to find the specified address!\n');
-  } else if (body.status === 'OK') {
-    console.log(`\nAddress: ${body.results[0].formatted_address}`);
-    console.log(`\nLatitutde: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}\n`);
-  }
-});
+geocode.geocodeAddress(inputAddress);
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,30 +1,28 @@
 const request = require('request');
 
 const geocodeAddress = (address) => {
-  console.log(address);
+
   const encodedInputAddress = encodeURIComponent(address);
-  console.log(encodedInputAddress);
 
   request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedInputAddress}`,
     json: true
   }, (err, res, body) => {
     if (err) {
-      console.log('\nUnable to connect to Google servers!');
+      console.log('\nUnable to connect to Google servers!\n');
     } else if (body.status === 'ZERO_RESULTS') {
-      console.log('\nUnable to find specified address!');
+      console.log('\nUnable to find specified address!\n');
     } else if (body.status === 'OK') {
       console.log(`\nAddress: ${body.results[0].formatted_address}`);
       console.log('---');
       console.log(`Lattitude: ${body.results[0].geometry.location.lat}`);
       console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-      console.log('---');
+      console.log('---\n');
     }
   })
+
 };
 
-geocodeAddress('2703 west 11 avenue vancouver');
-
-module.exports({
+module.exports = {
   geocodeAddress
-});
+};
