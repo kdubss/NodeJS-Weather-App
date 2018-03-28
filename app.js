9a2f65f24791cba4ef2f9b8e116525b42bd21ec8
Kang Wang <kang.wang@hakai.org>
2018-03-28 12:31:32 -0700

    Function to convert temperature from farenheit to celsius

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+//   .options({
+//     a: {
+//       demand: true,
+//       alias: 'address',
+//       describe: 'Set your current address to fetch the weather for.',
+//       string: true // always parse the 'a' flag as a string.
+//     }
+//   })
+//   .help()
+//   .alias('help', 'h')
+//   .argv;
+//
+// const inputAddress = argv.a;
+//
+// geocode.geocodeAddress(inputAddress, (err, res) => {
+//   if (err) {
+//     console.log('\nThere are errors in your request!\n', err, '\n');
+//   } else {
+//     console.log(JSON.stringify(res, undefined, 2));
+//   }
+// });
+//
+// // DarkSky API key: 87313e54274f92c50b1c5d843d7471dc
+// // DarkSky API Forecat Request URL: https://api.darksky.net/forecast/[key]/[latitude],[longitude]
 
-const geocode = require('./geocode/geocode');
+// https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/49.2624389,-123.1665417
 
-const argv = yargs
-  .options({
-    a: {
-      demand: true,
-      alias: 'address',
-      describe: 'Set your current address to fetch the weather for.',
-      string: true // always parse the 'a' flag as a string.
-    }
-  })
-  .help()
-  .alias('help', 'h')
-  .argv;
+const request = require('request');
 
-const inputAddress = argv.a;
+const url = 'https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/49.2624389,-123.1665417'
 
-geocode.geocodeAddress(inputAddress, (err, res) => {
+const farenheitToCelsius = (temperatureInFarenheit) => {
+  const tempInCelsius = (temperatureInFarenheit - 32) * (5/9);
+  return tempInCelsius;
+};
+
+request({
+  url,
+  json: true
+}, (err, res, body) => {
   if (err) {
-    console.log('\nThere are errors in your request!\n', err, '\n');
+    console.log('\nUnable to connect with DarkSky API!\n');
   } else {
-    console.log(JSON.stringify(res, undefined, 2));
+    // console.log(body);
+    console.log('---');
+    console.log('Current temperature in Vancouver is', body.currently.temperature);
   }
 });
