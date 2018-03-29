8a7fba5974983190f070691500b601f668bad4cc
Kang Wang <kang.wang@hakai.org>
2018-03-29 15:21:19 -0700

    Updated w/ axios module & setting up request to weather API Promise.

--- app-promise.js
+++ app-promise.js
@@ -1,4 +1,5 @@
 const yargs = require('yargs');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -13,4 +14,26 @@ const argv = yargs
   .alias('help', 'h')
   .argv;
 
-  
+const encodedInputAddress = encodeURIComponent(argv.address);
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedInputAddress}`;
+
+// Axios get method --> allows you to make the url request:
+// This .get() method returns a Promise!
+axios.get(geocodeUrl)
+  .then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('\nUnable to find specified address!\n');
+    }
+
+    const lat = response.data.results[0].geometry.location.lat;
+    const lng = response.data.results[0].geometry.location.lng;
+    const weatherUrl = `https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/${lat},${lng}`
+    console.log(response.data.results[0].formatted_address);
+  })
+  .catch((error) => {
+    if (error.code === 'ENOTFOUND') {
+      console.log('\nUnable to connect to Google Maps API servers!\n');
+    } else {
+      console.log(error.message);
+    }
+  });
