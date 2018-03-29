bdc5cfd23944b398e61fe9266dd412783b5e63a0
Kang Wang <kang.wang@hakai.org>
2018-03-28 23:07:20 -0700

    Imported and testing 'getWeather()' function; testing 'chaining callbacks'.

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-// const yargs = require('yargs');
-//
-// const geocode = require('./geocode/geocode');
-//
-// const argv = yargs
-//   .options({
-//     a: {
-//       demand: true,
-//       alias: 'address',
-//       describe: 'Set your current address to fetch the weather for.',
-//       string: true // always parse the 'a' flag as a string.
-//     }
-//   })
-//   .help()
-//   .alias('help', 'h')
-//   .argv;
-//
-// const inputAddress = argv.a;
-//
+const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
+const weather = require('./weather/weather');
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Set your current address to fetch the weather for.',
+      string: true // always parse the 'a' flag as a string.
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const inputAddress = argv.a;
+
 // geocode.geocodeAddress(inputAddress, (err, res) => {
 //   if (err) {
 //     console.log('\nThere are errors in your request!\n', err, '\n');
@@ -24,31 +25,14 @@
 //     console.log(JSON.stringify(res, undefined, 2));
 //   }
 // });
-//
-// // DarkSky API key: 87313e54274f92c50b1c5d843d7471dc
-// // DarkSky API Forecat Request URL: https://api.darksky.net/forecast/[key]/[latitude],[longitude]
 
-// https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/49.2624389,-123.1665417
+const lat = 49.2624389;
+const lon = -123.1665417;
 
-const request = require('request');
-
-const url = 'https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/49.2624389,-123.1665417'
-
-const farenheitToCelsius = (temperatureInFarenheit) => {
-  const tempInCelsius = (temperatureInFarenheit - 32) * (5/9);
-  return tempInCelsius;
-};
-
-// Takes 2 inputs (an options object), and a callback
-request({
-  url,
-  json: true
-}, (err, res, body) => {
-  if (!err && res.statusCode === 200) {
-    console.log('\n----');
-    console.log('Current temperature (deg.C) in Vancouver is:', farenheitToCelsius(body.currently.temperature));
-    console.log('----\n');
+weather.getWeather(lat, lon, (err, res) => {
+  if (err) {
+    console.log('\nThere are errors in your current request!\n', err, '\n');
   } else {
-    console.log('There is an error(s).  Unable to connect with \'forecast.io\' API or 404!\n');
+    console.log(JSON.stringify(res, undefined, 2));
   }
 });
