fa436045a3df81869aa7408be487686c7f39f49b
Kang Wang <kang.wang@hakai.org>
2018-03-28 23:08:10 -0700

    Writing the request to forecast.io API into getWeather() function in separate file.

---
+++ weather/weather.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const url = 'https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/49.2624389,-123.1665417'
+
+const farenheitToCelsius = (temperatureInFarenheit) => {
+  const tempInCelsius = (temperatureInFarenheit - 32) * (5/9);
+  return tempInCelsius;
+};
+
+const getWeather = (lat, lon, callback) => {
+  // Want the latitude, longitude, callback(errMsg, res)
+  // Takes 2 inputs (an options object), and a callback (in the request() function)
+  request({
+    url: `https://api.darksky.net/forecast/87313e54274f92c50b1c5d843d7471dc/${lat},${lon}`,
+    json: true
+  }, (err, res, body) => {
+    if (err) {
+      callback('\nUnable to connect to the "forecast.io" API!');
+    } else if (res.statusCode === 200) {
+      callback(undefined, {
+        temperature_degC: farenheitToCelsius(body.currently.temperature),
+        temperature_degF: body.currently.temperature,
+        
+      });
+    }
+  });
+};
+
+module.exports.getWeather = getWeather;
