ce3fb0d46aa2fce022a7f05aaec0bffba5489f5e
Kang Wang <kang.wang@hakai.org>
2018-03-30 12:37:13 -0700

    Function to retrieve lat/lon from input address.

--- python/geocode.py
+++ python/geocode.py
@@ -4,15 +4,28 @@ import pandas as pd
 
 from urllib.parse import quote
 
-googleMapsAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
 address = 'Kitsilano Vancouver'
-encodedInputAddress = quote(address)
-concatedUrl = googleMapsAPI + encodedInputAddress
 
-r = requests.get(concatedUrl);
+def getLatLon(inputAddress):
+    '''
+    Function to fetch the latitude, longitude from the Google Maps API
+    using tne stdin of 'inputAddress'
+    '''
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    encodedInputAddress = quote(inputAddress)
+    apiAddress = url + encodedInputAddress
 
-print(googleMapsAPI)
-print(address)
-print(encodedInputAddress)
-print(concatedUrl)
-print('\nStatus code of request: %i' % r.status_code)
+    r = requests.get(apiAddress)
+    lat = r.json()['results'][0]['geometry']['location']['lat']
+    lon = r.json()['results'][0]['geometry']['location']['lng']
+
+    return lat, lon
+
+    
+
+# > For Testing:
+# print(googleMapsAPI)
+# print(address)
+# print(encodedInputAddress)
+# print(concatedUrl)
+# print('\nStatus code of request: %i' % r.status_code)
