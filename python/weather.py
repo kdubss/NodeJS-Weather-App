fa105d821fe95f693215f24cbcefb76817dee668
Kang Wang <kang.wang@hakai.org>
2018-03-30 17:44:30 -0700

    Indices of dict keys of the json returned from forecast.io

--- python/weather.py
+++ python/weather.py
@@ -1,6 +1,10 @@
 # ./weather-app/python/weather.py
-import pandas as pd
+'''
+Module to parse the JSON weather data from forecast.io (darksky.net) and to
+make data manipulaions by using Pandas.
+'''
 import requests
+import pandas as pd
 
 from local_settings import env
 from geocode import getLatLon
@@ -13,9 +17,15 @@ def getJSONWeatherData(inputAddress):
     url = 'https://api.darksky.net/forecast/%s/%s,%s' % (env['forecastApiKey'],
                                                          str(lat), str(lon))
     r = requests.get(url)
-    return r.json()
+    return r
 
 if __name__ == '__main__':
 
     address = 'Kitsilano Vancouver'
-    print(getJSONWeatherData(address))
+    json_data = getJSONWeatherData(address)
+    json_dict = json_data.json()
+
+    print('\nKeys in \'json_data\' dict object:\n')
+    for ind, each_key in enumerate(json_dict.keys()):
+        print(ind + 1, '\t', each_key)
+    print('\n')
