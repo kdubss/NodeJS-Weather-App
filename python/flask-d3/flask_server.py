b41039932c5cb31b7ae2fc727fb5178e6fea39d8
Kang Wang <kang.wang@hakai.org>
2018-04-03 11:45:31 -0700

    Added error handling messages and testing external api calls within flask

--- python/flask-d3/flask_server.py
+++ python/flask-d3/flask_server.py
@@ -1,8 +1,13 @@
 # ./weather-app/python/flask_server.py
-import os
+import os, sys
 import datetime as dt
 
 from flask import Flask, request, session, flash, render_template, redirect
+
+sys.path.insert(0, '../')
+import api_requests as dsky
+
+from geocode import getLatLon
 
 app = Flask(__name__)
 
@@ -10,10 +15,27 @@ app = Flask(__name__)
 def hello():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def url_err(err):
+    return '''
+        Invalid URL end-point!
+    '''
+
+@app.errorhandler(500)
+def server_err(err):
+    return '''
+        Internal server error!
+    '''
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/temperature')
+def show_temp_d3():
+    forecast = dsky.getForecastDataFromDarkSkyAPI('Vancouver').json()
+    
+
 if __name__ == '__main__':
 
-    app.run(port = 8080)
+    app.run(port = 8080, debug = True)
