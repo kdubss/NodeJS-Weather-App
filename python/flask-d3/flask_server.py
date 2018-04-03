b1d5230d0f17caf76d6f057edcff89e2aac72507
Kang Wang <kang.wang@hakai.org>
2018-04-02 23:39:31 -0700

    Moved flask_server.py & constructed bare-bones index html view

---
+++ python/flask-d3/flask_server.py
@@ -0,0 +1,15 @@
+# ./weather-app/python/flask_server.py
+import os
+import datetime as dt
+
+from flask import Flask, request, session, flash, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+
+    app.run(port = 8080)
