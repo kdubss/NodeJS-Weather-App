31d3f0808196443810763053f84ce74f6f9143a6
Kang Wang <kang.wang@hakai.org>
2018-04-04 16:44:18 -0700

    Loaded csv. data; changed 'string' temperature values into numerical values by calling on the unary operator through the .row() method, which allows formatting of every row of the data file.

--- d3/d3-loading-csv-data.js
+++ d3/d3-loading-csv-data.js
@@ -1,3 +1,17 @@
-d3.csv('./csv/forecast-hourly-temp.csv').get((err, data) => {
-  console.log(data);
-});
+// ./weather-app/d3
+// - Loading .csv data of the weather data fetched from DarkSky API.
+
+d3.csv('./csv/forecast-hourly-temp.csv')
+  .row((data) => {
+    return {
+      date: data.datetime,
+      temperature: +data.temperature
+    };
+  })
+  .get((err, data) => {
+    if (err) {
+      console.log('\nErrors:\n', err);
+    } else {
+      console.log(data);
+    }
+  });
