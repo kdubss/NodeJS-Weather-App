1f30048feff21bd534246cd63664d86ad78f1e8e
Kang Wang <kang.wang@hakai.org>
2018-04-05 10:37:54 -0700

    Reformatted date time from '%d %b %H' to '%d-%b-%y %H:%M:%S'.

--- python/api_requests.py
+++ python/api_requests.py
@@ -222,6 +222,6 @@ def convertUnixTime2PST(unix_timestamp):
     '''
     Function to convert unix time stamp to PST time (localy here, in Vancouver)
     '''
-    pretty_time = time.strftime('%d %b %H:%M',
+    pretty_time = time.strftime('%d-%b-%y %H:%M:%S',
                                 time.localtime(unix_timestamp))
     return pretty_time
