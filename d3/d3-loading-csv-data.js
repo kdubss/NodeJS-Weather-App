4ee93ea279911e13a8b1e71108dc48ab78ed5108
Kang Wang <kang.wang@hakai.org>
2018-04-04 17:04:15 -0700

    Defined the x/y scales & defined the X/Y Axis based on the respective scales.

--- d3/d3-loading-csv-data.js
+++ d3/d3-loading-csv-data.js
@@ -19,9 +19,24 @@ d3.csv('./csv/forecast-hourly-temp.csv')
       const maxTemp = d3.max(data, (d) => { return d.temperature; });
       const minTemp = d3.min(data, (d) => { return d.temperature; });
 
-      console.log('max. Date:', maxDate);
-      console.log('min. Date:', minDate);
-      console.log('max. Temp:', maxTemp);
-      console.log('min. Temp:', minTemp);
+      // console.log('max. Date:', maxDate);
+      // console.log('min. Date:', minDate);
+      // console.log('max. Temp:', maxTemp);
+      // console.log('min. Temp:', minTemp);
+
+      // Defining x,y-scales:
+      const y = d3.scaleLinear()
+                  .domain([0, maxTemp])
+                  .range([height, 0]);
+      const x = d3.scaleTime()
+                  .domain([minDate, maxDate])
+                  .range([0, width]) // width
+
+      // Defining X,Y-Axis:
+      const yAxis = d3.axisLeft(y); // defining y-ax with the y-scaling (see above)
+      const xAxis = d3.axisBottom(x); // defining the x-ax with the x-scaling (see above)
+
+      
+
     }
   });
