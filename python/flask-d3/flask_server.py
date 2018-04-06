37e1edb70b8c9d707f2ae09ac8f27eb0b9dd8391
Kang Wang <kang.wang@hakai.org>
2018-04-06 15:40:58 -0700

    Updated end-points on flask server and edited routing links in index.html; created static html files for forecast and hindcast data to be rendered by D3.

--- python/flask-d3/flask_server.py
+++ python/flask-d3/flask_server.py
@@ -13,6 +13,59 @@ import api_requests as api
 
 app = Flask(__name__)
 
+@app.errorhandler(404)
+def pageNotFound(err):
+    '''
+    Handles a 404 client-side error(s).
+    '''
+    return (
+        '''
+        Sorry, 404!
+        This means it's completely YOUR fault (i.e. wrong url, etc.).
+
+        Make sure to get'yo stuff together and do it right!
+        '''
+    )
+
+@app.errorhandler(500)
+def serverError(err):
+    '''
+    Handles a 500 server-side error(s).
+    '''
+    return (
+        '''
+        Okay okay, okay...
+
+        This is our fault...we'll do our best to get our stuff right, so you
+        can get'yo stuff right!
+        '''
+    )
+
+@app.route('/')
+def getIndexPage():
+    '''
+    Rendering './templates/index.html'.
+    '''
+    return render_template('index.html')
+
+@app.route('/forecast')
+def getForeacastTemperatureD3():
+    '''
+    Parsing data and rendering the forecasted temperature data, then passing
+    the data to ./templates/forecast_temperature.html, in which the data will
+    be rendered by D3.
+    '''
+    return render_template('forecast_temperature.html')
+
+@app.route('/hindcast')
+def getHistoricalHindcastTemperatureD3():
+    '''
+    Parsing data and rendering the historical hindcast temperature data, then passing
+    the data to ./templates/historical_temperature.html, in which the data will
+    be rendered by D3.
+    '''
+    return render_template('historical_temperature.html')
+
 @app.route('/temperature')
 def getForecastAndHindcastTemperatureD3():
     '''
