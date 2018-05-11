5af4689a2fae009c6197e3b9ee43e4b69b5424be
Kang Wang <kang.wang@hakai.org>
2018-05-11 09:31:40 -0700

    Established two different routes (index & local weather conditions).

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ const app = express();
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
+app.get('/', (req, res) => {
+  res.send('Index Page');
+});
+
+app.get('/local', (req, res) => {
+  res.send('Current Local Weather');
+});
+
 app.listen(PORT, () => {
   console.log(`\nDark Sky Weather now running on LOCAL PORT ${PORT}!`);
 });
